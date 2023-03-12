6c49bb9ca60d4cec85a26918413f96c161b63054
JP <67994969+dasma4114@users.noreply.github.com>
2023-03-12 16:39:34 -0700

    Initialized flask

---
+++ app.py
@@ -0,0 +1,13 @@
+import datetime
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formattted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        print(entry_content, formattted_date)
+    return render_template("home.html")
